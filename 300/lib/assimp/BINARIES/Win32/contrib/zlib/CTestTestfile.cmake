3e6659a412f8ab36a1f1eba56d9ade79afd9d5ff
Taterr <richmondch001@gmail.com>
2023-08-18 00:20:53 +0800

    assimp and meshoptimizer libraries

---
+++ 300/lib/assimp/BINARIES/Win32/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: F:/RTIS/Y3T1/CSD2150_r.choo_submission/dependencies/assimp/contrib/zlib
+# Build directory: F:/RTIS/Y3T1/CSD2150_r.choo_submission/dependencies/assimp/BINARIES/Win32/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
