c932edd055aed2100c770aea09da7061e2063ec0
rollypolly06 <lfsroy98@outlook.com>
2023-08-26 16:46:21 +0800

    changed blend mode

--- 300/_GRAPHICS/demo/DemoScene.cpp
+++ 300/_GRAPHICS/demo/DemoScene.cpp
@@ -48,7 +48,7 @@ void GFX::DemoScene::Initialize()
 
     // State
     glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
     glEnable(GL_DEPTH_TEST);
