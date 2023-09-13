2f713fd0aa723edaee512b414f570d75f3d322d7
Hapemo <j.teoh@digipen.edu>
2023-09-13 13:35:02 +0800

    Update ECS_Systems.cpp

--- 300/_ENGINE/source/ECS/ECS_Systems.cpp
+++ 300/_ENGINE/source/ECS/ECS_Systems.cpp
@@ -1,17 +1,8 @@
 #include "Physics/PhysicsSystem.h"
 #include "ECS/ECS_Systems.h"
 #include "ScriptingSystem.h"
-#include "rttr/registration.h"
 
 SystemManager* systemManager;
-
-static void f() { std::cout << "Hello World" << std::endl; }
-
-RTTR_REGISTRATION
-{
-		using namespace rttr;
-		registration::method("f", &f);
-}
 
 SystemManager::SystemManager()
 {
@@ -28,7 +19,6 @@ void SystemManager::Init()
 {
 	mPhysicsSystem.get()->Init();
 	mScriptingSystem.get()->Init();
-	rttr::type::invoke("f", {});
 }
 
 void SystemManager::Update(float dt)
