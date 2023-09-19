0b49bb704f95cefc5916bd1bedc281ea2e002f1f
Taterr <90663945+Taterr@users.noreply.github.com>
2023-09-19 21:06:05 +0800

    compilers.bat -- texturescompressor

--- 300/Compilers.bat
+++ 300/Compilers.bat
@@ -25,8 +25,8 @@ if %errorlevel% equ 0 (
 	echo Build Failed.
 )
 
-:: change the working directory to the geomcompiler DEBUG filepath
-cd %cd%/x64/Release		
+:: change the working directory to the Texture Compressor Filepath
+cd %cd%/_GEOM_COMPILER
 _GEOM_COMPILER.exe
 
 if %ERRORLEVEL% GEQ 1 goto :PAUSE
@@ -35,9 +35,27 @@ powershell write-host -fore White Done Compiling Geom
 powershell write-host -fore White ------------------------------------------------------------------------------------------------------
 
 :: change the batchfile working directory back to the original filepath
-cd /d %BATCHFILE_PATH%	
+cd %BATCHFILE_PATH%
 if %ERRORLEVEL% GEQ 1 goto :PAUSE
 :: //////////////////////////////////////////////////////////GEOM COMPILER////////////////////////////////////////////////////////////////
+
+
+:: //////////////////////////////////////////////////////////TEXTURE COMPILER////////////////////////////////////////////////////////////////
+:Texture_Compressor
+powershell write-host -fore White ------------------------------------------------------------------------------------------------------
+powershell write-host -fore White COMPILING TEXTURE_COMPRESSOR.EXE
+powershell write-host -fore White ------------------------------------------------------------------------------------------------------
+echo.
+
+:: change the working directory to the Texture Compressor Filepath
+cd ../_TEXTURE_COMPRESSOR
+_TEXTURE_COMPRESSOR.exe
+
+if %ERRORLEVEL% GEQ 1 goto :PAUSE
+powershell write-host -fore White ------------------------------------------------------------------------------------------------------
+powershell write-host -fore White Done Compiling Textures
+powershell write-host -fore White ------------------------------------------------------------------------------------------------------
+:: //////////////////////////////////////////////////////////TEXTURE COMPILER////////////////////////////////////////////////////////////////
 
 :: finished the batch file.
 exit	
