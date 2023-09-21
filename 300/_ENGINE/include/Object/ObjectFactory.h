cb992637c3f27aeb0eb5a1b890111def75db0beb
charissayip <charissa.yip@digipen.edu>
2023-09-21 17:25:23 +0800

    removed for loop + some gamestate function updates

--- 300/_ENGINE/include/Object/ObjectFactory.h
+++ 300/_ENGINE/include/Object/ObjectFactory.h
@@ -13,12 +13,12 @@ public:
 
 	// *this refers to Scene pointer
 	//serializationManager->LoadScene(*this, _path); TODO
-	static void LoadScene(Scene* ptr, const std::string& filename);
+	static void LoadScene(Scene* scene, const std::string& filename);
 	// deserialize path
 	// load stuff and push into *this->mEntities
 
 	//serializationManager->SaveScene(*this); TODO
-	static void SaveScene(Scene* ptr);
+	static void SaveScene(Scene* scene);
 	// pass in Scene pointer
 	// loop thru the mEntities container and serialize
 	// *this->mName + ".json"
@@ -26,10 +26,10 @@ public:
 
 	// *this refers to GameState pointer
 	//serializationManager->LoadGameState(*this, _path); TODO
-	static void LoadGameState(GameState* ptr, const std::string& filename);
+	static void LoadGameState(GameState* gs, const std::string& filename);
 
 	//serializationManager->SaveGameState(*this); TODO
-	static void SaveGameState(GameState* ptr);
+	static void SaveGameState(GameState* gs);
 
 	//static void DeserializePrefab(const std::string& filename); // just use the normal deserializescene function, works exactly the same
 	static void SerializePrefab(Entity e, const std::string& filename);
