2e0129e722b384cb3ef906bae949c735b0235759
saladpotato <90621700+saladpotato@users.noreply.github.com>
2023-09-26 21:52:02 +0800

    [InputMapSystem] - For Now

--- 300/_TOOLS/source/Input/InputMapSystem.cpp
+++ 300/_TOOLS/source/Input/InputMapSystem.cpp
@@ -3,20 +3,25 @@
 
 void InputMapSystem::Init()
 {
-	InputActionMap general_movement = InputActionMap("General Movement"); 
+	InputActionMap general_movement = InputActionMap("General Movement");
 	general_movement.Enable();
 	general_movement.AddAction("move");
+	// general_movement.AddAction("jump");
 
 	InputAction& move_action = general_movement.FindAction("move");
 	move_action.Enable();
 	move_action.AddKeyBinding(KEY_BIND::KEY_UP, E_STATE::PRESS, E_KEY::UP);
-	
-	AddActionMap(general_movement);
+	move_action.AddKeyBinding(KEY_BIND::KEY_DOWN, E_STATE::PRESS, E_KEY::DOWN);
+	move_action.AddKeyBinding(KEY_BIND::KEY_LEFT, E_STATE::PRESS, E_KEY::LEFT);
+	move_action.AddKeyBinding(KEY_BIND::KEY_RIGHT, E_STATE::PRESS, E_KEY::RIGHT);
 
-	//CheckActionInput(move_action, E_KEY::UP);
+	glm::vec2 vec_response_up = move_action.ReadValue(E_KEY::UP);
+	glm::vec2 vec_response_down = move_action.ReadValue(E_KEY::DOWN);
+	glm::vec2 read_vec;
 
-	glm::vec2 vec_response = move_action.ReadValue(E_KEY::UP);
+	E_KEY key_pressed = E_KEY::DOWN;
 
+	read_vec = move_action.ReadValue(key_pressed);
 }
 
 void InputMapSystem::Update()
@@ -39,7 +44,6 @@ void InputMapSystem::Update()
 
 					E_KEY   key_bind = binding_pair.second.key_binding;
 					E_STATE key_state = binding_pair.second.key_state;
-
 
 					if (Input::CheckKey(key_state, key_bind))
 					{
