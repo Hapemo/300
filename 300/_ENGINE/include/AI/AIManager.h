f05812ca11d66a6594a12b21882e72463c1be5d6
Hapemo <j.teoh@digipen.edu>
2023-10-18 11:45:00 +0800

    AI Manager set up

---
+++ 300/_ENGINE/include/AI/AIManager.h
@@ -0,0 +1,64 @@
+#pragma once
+#include "pch.h"
+
+struct Entity;
+
+enum class E_MOVEMENT_TYPE {
+	GROUND,
+	AIR
+};
+
+enum class E_ATTACK_TYPE {
+	CONTACT,
+	SHOOTING
+};
+
+struct EnemySetting {
+	E_MOVEMENT_TYPE mMovementType;	// Enemy's movement type
+	E_ATTACK_TYPE mAttackType;			// Enemy's attack type
+	float mSpreadOut;								// Degree of spreading out
+};
+
+class AIManager {
+public:
+	/*!*****************************************************************************
+	Get the direction of entity which movement is dictated by AI.
+
+	\param _e
+	- Entity to find direction of
+
+	\return glm::vec3
+	- Direction of movement dictated by AI
+	*******************************************************************************/
+	glm::vec3 GetDirection(Entity _e);
+
+	/*!*****************************************************************************
+	Get the next position the AI will be in.
+
+	\param _e
+	- Entity to find direction of
+
+	\return glm::vec3
+	- Next position of the entity dictated by AI
+	*******************************************************************************/
+	glm::vec3 GetNextPosition(Entity _e);
+
+private:
+	
+
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
---
+++ 300/_ENGINE/source/AI/AIManager.cpp
@@ -0,0 +1,27 @@
+#include "AI/AIManager.h"
+#include "ECS/ECS.h"
+
+
+//--------------------------------------------------
+// Public functions
+//--------------------------------------------------
+glm::vec3 AIManager::GetDirection(Entity _e) {
+
+}
+
+glm::vec3 AIManager::GetNextPosition(Entity _e) {
+
+}
+
+
+
+//--------------------------------------------------
+// Private functions
+//--------------------------------------------------
+
+
+
+
+
+
+
