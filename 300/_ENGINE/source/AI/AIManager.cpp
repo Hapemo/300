638e1a075003a50b89c32b19ff083999c5884102
Hapemo <j.teoh@digipen.edu>
2023-10-18 12:18:12 +0800

    Sparseset

--- 300/_ENGINE/source/AI/AIManager.cpp
+++ 300/_ENGINE/source/AI/AIManager.cpp
@@ -6,11 +6,11 @@
 // Public functions
 //--------------------------------------------------
 glm::vec3 AIManager::GetDirection(Entity _e) {
-
+	return glm::vec3();
 }
 
 glm::vec3 AIManager::GetNextPosition(Entity _e) {
-
+	return glm::vec3();
 }
 
 
