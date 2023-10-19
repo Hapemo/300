2208ca43601b7925c698ec647fdf636db44c03d0
kewyj <74573283+kewyj@users.noreply.github.com>
2023-10-19 15:51:39 +0800

    update positions

--- 300/_ENGINE/source/Physics/PhysicsSystem.cpp
+++ 300/_ENGINE/source/Physics/PhysicsSystem.cpp
@@ -41,13 +41,25 @@ void PhysicsSystem::Update(float dt)
 
 	for (auto itr = mActors.begin(); itr != mActors.end(); ++itr)
 	{
+		// retrieving positions in physx
 		physx::PxTransform PXform = itr->second->getGlobalPose();
-		Transform& xform = Entity(itr->first).GetComponent<Transform>();
+		Entity e = Entity(itr->first);
+		Transform& xform = e.GetComponent<Transform>();
+
+		// overwriting entities with updated positions
 		xform.mTranslate = Convert(PXform.p);
-		if (Entity(itr->first).HasComponent<BoxCollider>())
-			xform.mTranslate -= Entity(itr->first).GetComponent<BoxCollider>().mTranslateOffset;
-		xform.mRotate = glm::vec3(0,0,0);
-		RigidBody& rbod = Entity(itr->first).GetComponent<RigidBody>();
+		xform.mRotate = glm::eulerAngles(Convert(PXform.q));
+		if (e.HasComponent<BoxCollider>())
+			xform.mTranslate -= e.GetComponent<BoxCollider>().mTranslateOffset;
+		else if (e.HasComponent<SphereCollider>())
+			xform.mTranslate -= e.GetComponent<SphereCollider>().mTranslateOffset;
+		else if (e.HasComponent<CapsuleCollider>())
+			xform.mTranslate -= e.GetComponent<CapsuleCollider>().mTranslateOffset;
+		else if (e.HasComponent<AABBCollider>())
+			xform.mTranslate -= e.GetComponent<AABBCollider>().mTranslateOffset;
+
+		// update velocity
+		RigidBody& rbod = e.GetComponent<RigidBody>();
 		if (rbod.mMotion != MOTION::DYNAMIC)
 			continue;
 		rbod.mVelocity = Convert(static_cast<physx::PxRigidDynamic*>(itr->second)->getLinearVelocity());
