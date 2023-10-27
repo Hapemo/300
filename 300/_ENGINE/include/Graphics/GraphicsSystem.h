ff8a5650eb210e9a35defa11ee6aff1215861bec
Taterr <richmondch001@gmail.com>
2023-10-27 20:07:18 +0800

    yj serialize pls ty

--- 300/_ENGINE/include/Graphics/GraphicsSystem.h
+++ 300/_ENGINE/include/Graphics/GraphicsSystem.h
@@ -239,13 +239,13 @@ public:
 	CAMERA_TYPE m_CameraControl;
 
 	// -- Bloom -- 
-	vec3		mAmbientBloomThreshold { 0.05, 0.05, 0.005 };
-	float		mAmbientBloomExposure{ 0.4f };
-	float		mTexelOffset{ 1.f };
-	float		mSamplingWeight{ 1.f };
+	vec3		mAmbientBloomThreshold { 0.05, 0.05, 0.005 };		// this yj
+	float		mAmbientBloomExposure{ 0.4f };						// this yj
+	float		mTexelOffset{ 1.f };								// this yj
+	float		mSamplingWeight{ 1.f };								// this yj
 
 	// -- Chromatic Abbreation --
-	float		mChromaticStrength{ 0.006f };
+	float		mChromaticStrength{ 0.006f };						// this yj
 
 	// -- Textures --
 	std::vector<int> m_Textures;	// 0, 1, ..., 31
@@ -259,8 +259,8 @@ public:
 	bool	m_EnableGlobalAnimations{ 1 };
 	bool	m_HasLight{ false };
 	bool    m_EnableScroll{ false };
-	bool    m_EnableBloom{ false };
-	bool	m_EnableChromaticAbberation{ false };
+	bool    m_EnableBloom{ false };									// this yj
+	bool	m_EnableChromaticAbberation{ false };					// this yj
 
 	// -- Stats --
 	int		m_LightCount{};
