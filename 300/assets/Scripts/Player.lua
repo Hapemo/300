21febc5be503259f927b2cb2cf458077ced58959
kewyj <74573283+kewyj@users.noreply.github.com>
2023-10-29 19:26:05 +0800

    player

--- 300/assets/Scripts/Player.lua
+++ 300/assets/Scripts/Player.lua
@@ -2,14 +2,11 @@ vec2 = Vec2.new()
 function Alive()
     gameStateSys = systemManager:mGameStateSystem();
     cameraEntity = gameStateSys:GetEntity("Camera", "testSerialization")
-    print(cameraEntity.id)
 end
 
 function Update()
     vec2 = Input.CursorPos()
     Camera_Scripting.RotateCameraView(cameraEntity, vec2)
-    print(vec2.x)
-    print(vec2.y)
 end
 
 function Dead()
