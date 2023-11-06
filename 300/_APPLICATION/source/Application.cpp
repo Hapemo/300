e3b1f972eaf3c1fa5368242193fbb7ba8f174f73
kewyj <74573283+kewyj@users.noreply.github.com>
2023-11-06 15:50:48 +0800

    remove comment

--- 300/_APPLICATION/source/Application.cpp
+++ 300/_APPLICATION/source/Application.cpp
@@ -62,157 +62,12 @@ void Application::SystemInit()
 {
     Input::mIsEditor = false;
 
-
-#pragma region AudioComponent Test
-    //Entity entAudio = systemManager->ecs->NewEntity();
-    //entAudio.AddComponent<Audio>({ "../assets/Audio" , "farm_ambience.wav", AUDIO_BGM, false});
-
-    //// Test Edit <Audio> Component
-    ////entAudio.GetComponent<Audio>().mFileName = "farm_ambience.wav";
-    ///*entAudio.GetComponent<Audio>().mIsPlay = false;
-    //entAudio.GetComponent<Audio>().mIsPlaying = false;
-    //entAudio.GetComponent<Audio>().mIsPlay = false;
-    //entAudio.GetComponent<Audio>().mIsPlaying = false;*/
-
-    //Entity radioAudio = systemManager->ecs->NewEntity();
-    //radioAudio.AddComponent<Audio>({ "../assets/Audio" , "tuning-radio-7150.wav" , AUDIO_SFX, false });
-
-    //Entity npcAudio = systemManager->ecs->NewEntity();
-    //npcAudio.AddComponent<Audio>({ "../assets/Audio" , "NPC_Greeting.wav" , AUDIO_SFX, true });
-
-    //Entity non_existent_audio = systemManager->ecs->NewEntity();
-    //non_existent_audio.AddComponent<Audio>();
-
-#pragma endregion
-
-#pragma region testphysics
-    //Entity e1 = systemManager->ecs->NewEntity();
-    //Entity e2 = systemManager->ecs->NewEntity();
-    //Entity e3 = systemManager->ecs->NewEntity();
-
-    //e1.AddComponent<RigidBody>();
-    //e1.GetComponent<RigidBody>().mDensity = 10.f;
-    //e1.GetComponent<RigidBody>().mMaterial = MATERIAL::CONCRETE;
-    //e1.GetComponent<RigidBody>().mMotion = MOTION::STATIC;
-    //e1.AddComponent<PlaneCollider>();
-
-    //e2.AddComponent<RigidBody>();
-    //e2.GetComponent<RigidBody>().mDensity = 10.f;
-    //e2.GetComponent<RigidBody>().mMaterial = MATERIAL::CONCRETE;
-    //e2.GetComponent<RigidBody>().mMotion = MOTION::DYNAMIC;
-    //e2.AddComponent<SphereCollider>();
-    //e2.GetComponent<Transform>().mTranslate.y = 400.f;
-
-    //e3.AddComponent<RigidBody>();
-    //e3.GetComponent<RigidBody>().mDensity = 10.f;
-    //e3.GetComponent<RigidBody>().mMaterial = MATERIAL::CONCRETE;
-    //e3.GetComponent<RigidBody>().mMotion = MOTION::STATIC;
-    //e3.AddComponent<SphereCollider>();
-    //e3.GetComponent<Transform>().mTranslate = glm::vec3(300.f, 105, 300.f);
-    //eee = (uint32_t)e2.id;
-
-#pragma endregion testphysics
   /*  systemManager->Init(false, &mWindow);*/ // Moved this after Audio Component Test
 
     systemManager->Init(false, &mWindow);
     FPSManager::Init();
     Input::Init(false);
-#pragma region testserialization
-    //ObjectFactory::DeserializeScene("../assets/Scenes/test.json");
 
-    //Entity ent1 = systemManager->ecs->NewEntity();
-    //Entity ent2 = systemManager->ecs->NewEntity();
-    //Entity ent3 = systemManager->ecs->NewEntity();
-
-    //ent1.GetComponent<General>().name = "Testing";
-    //ent1.GetComponent<General>().isActive = true;
-    //ent1.GetComponent<General>().tag = TAG::PLAYER;
-    //ent1.GetComponent<General>().subtag = SUBTAG::ACTIVE;
-    //ent1.AddComponent<Scripts>();
-    //Script test, test2;
-    //test.scriptFile = "../assets/Scripts/Run.lua";
-    //test.env = { systemManager->mScriptingSystem->luaState, sol::create, systemManager->mScriptingSystem->luaState.globals() };
-    //test2.scriptFile = "../assets/Scripts/Fly.lua";
-    //test2.env = { systemManager->mScriptingSystem->luaState, sol::create, systemManager->mScriptingSystem->luaState.globals() };
-    //ent1.GetComponent<Scripts>().scriptsContainer.push_back(test);
-    //ent1.GetComponent<Scripts>().scriptsContainer.push_back(test2);
-
-    //ent2.AddComponent<Audio>();
-
-    //ent2.GetComponent<General>().name = "Other";
-    //ent2.GetComponent<General>().isActive = true;
-    //ent2.GetComponent<General>().tag = TAG::UNKNOWN;
-    //ent2.GetComponent<General>().subtag = SUBTAG::BACKGROUND;
-
-    //ent3.GetComponent<General>().name = "Apple";
-    //ent3.GetComponent<General>().isActive = false;
-    //ent3.GetComponent<General>().tag = TAG::UNKNOWN;
-    //ent3.GetComponent<General>().subtag = SUBTAG::ACTIVE;
-    //
-
-
-    //ObjectFactory::SerializeScene("../assets/Scenes/test.json");
-
-    //systemManager->mGameStateSystem->NewGameState("MAMAMA");
-    //systemManager->mGameStateSystem->GetCurrentGameState()->AddScene("test");
-    //systemManager->mGameStateSystem->GetCurrentGameState()->Save();
-
-    //auto view = systemManager->ecs->GetEntitiesWith<General, Transform>();
-    //int size = view.size();
-    //for (Entity e : view)
-    //{
-    //    e.GetComponent<Transform>();
-    //}
-#pragma endregion testserialization
-#pragma region testparentchild
-
-    //loop over children of an entity
-    //Entity ent4 = systemManager->ecs->NewEntity();
-    //Entity ent5 = systemManager->ecs->NewEntity();
-    //Entity ent6 = systemManager->ecs->NewEntity();
-    //Entity ent7 = systemManager->ecs->NewEntity();
-
-    //ent4.AddChild(ent5);
-    //int s1 = ent4.GetComponent<Children>().mNumChildren;
-
-    //ent4.AddChild(ent6);
-    //ent4.AddChild(ent7);
-    //s1 = ent4.GetComponent<Children>().mNumChildren;
-
-
-    //auto viewtemp1 = systemManager->ecs->GetEntitiesWith<Parent>();
-    //int size1 = viewtemp1.size();
-    //auto viewtemp2 = systemManager->ecs->GetEntitiesWith<Parent, Children>();
-    //bool e4 = ent4.HasAllOfComponents<Parent, Children>();
-    //bool e5 = ent5.HasAllOfComponents<Parent, Children>();
-    //bool e6 = ent6.HasAllOfComponents<Parent, Children>();
-    //std::vector<Entity> children = ent5.GetAllChildren();
-    //Entity ent5parent = ent5.GetParent();
-    //Entity ent6parent = ent6.GetParent();
-    //ent4.RemoveChild(ent7);
-    //auto v1 = systemManager->ecs->GetEntitiesWith<Parent>();
-    //assert(ent5.GetComponent<Parent>().mNextSibling == ent5.GetComponent<Parent>().mPrevSibling);
-    //assert(ent6.GetComponent<Parent>().mNextSibling == ent6.GetComponent<Parent>().mPrevSibling);
-
-    //s1 = ent4.GetComponent<Children>().mNumChildren;
-    //assert( == 2);
-
-#pragma endregion testparentchild
-#pragma region testprefab
-    //Entity ent00 = systemManager->ecs->NewEntity();
-    //ent00.GetComponent<General>().name = "testPrefab";
-    //ent00.AddComponent<RigidBody>();
-    //ent00.AddComponent<PlaneCollider>();
-    //systemManager->ecs->NewPrefab(ent00);
-
-    //
-    //Entity ent01 = systemManager->ecs->NewEntityFromPrefab("testPrefab");
-    //systemManager->ecs->UnlinkPrefab(ent01);
-    //ent01.RemoveComponent<RigidBody>();
-    //ent01.RemoveComponent<PlaneCollider>();
-    //systemManager->ecs->NewPrefab(ent01);
-
-#pragma endregion testprefab
 
 
 
@@ -224,6 +79,9 @@ void Application::MainUpdate()
 {
     while (!systemManager->mGameStateSystem->Exited())
     {
+        std::string title{ "Application | FPS: " };
+        title += std::to_string((int)FPSManager::fps);
+        systemManager->GetWindow()->SetWindowTitle(title.c_str());
         FirstUpdate();
         SystemUpdate();
         SecondUpdate(); // This should always be the last
