7cb7b9521f8ca20358bd25a384793b61ccc01367
aader320 <han.domain1@gmail.com>
2023-11-15 19:30:13 +0800

    (some fixes)

---
+++ 300/assets/Scripts/crosshair-1.lua
@@ -0,0 +1,31 @@
+d_position = new.vec2(-146,146)
+m_position = new.vec2()
+de_speed = 10
+add_speed = 1
+
+
+function Alive()
+
+end
+
+function Update()
+    if(inputMapSys:GetButtonDown("Mouse")) then
+
+    end
+end
+
+function Dead()
+
+end
+
+function OnTriggerEnter(Entity)
+    
+end
+
+function OnTrigger(Entity)
+    
+end
+
+function OnTriggerExit(Entity)
+    
+end
