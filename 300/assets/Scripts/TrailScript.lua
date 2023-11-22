2d4baeb4afb208a1223377c873792ed8f055e9d1
kewyj <74573283+kewyj@users.noreply.github.com>
2023-11-22 14:06:36 +0800

    parricle

---
+++ 300/assets/Scripts/TrailScript.lua
@@ -0,0 +1,69 @@
+local timer
+local this
+local maxTime
+local eScale = Vec3.new()
+local eTranslate = Vec3.new()
+local originalTranslate = Vec3.new()
+local originalScale = Vec3.new()
+local scalePerSec
+local finalTranslate = Vec3.new()
+local amplitude
+local max
+function Alive()
+    --lifespan
+    maxTime = 5.0
+    timer = 0.0
+
+    --randomness
+    max = 2.0
+    amplitude = math.random() + math.random(0, max)
+    
+    --transform
+    this = Helper.GetScriptEntity(script_entity.id)
+    originalTranslate = this:GetTransform().mTranslate
+    originalScale = this:GetTransform().mScale
+    
+    --scale changes per second
+    scalePerSec = originalScale.x / maxTime
+end
+
+function Update()
+    timer = FPSManager.GetDT() + timer
+    
+    --random movement
+    eTranslate = this:GetTransform().mTranslate
+    finalTranslate.x = originalTranslate.x + amplitude * math.sin(timer)
+    finalTranslate.y = originalTranslate.y + amplitude * math.sin(timer) 
+    finalTranslate.z = originalTranslate.z + amplitude * math.sin(timer) 
+    eTranslate.x = finalTranslate.x
+    eTranslate.y = finalTranslate.y
+    eTranslate.z = finalTranslate.z
+    
+    --scale decrement
+    scaleDecrement = FPSManager.GetDT() * scalePerSec
+    eScale = this:GetTransform().mScale
+    eScale.x = eScale.x - scaleDecrement
+    eScale.y = eScale.y - scaleDecrement
+    eScale.z = eScale.z - scaleDecrement
+    print("scale"..amplitude * math.sin(timer))
+
+    if (eScale.x < 0) then
+        systemManager.ecs:SetDeleteEntity(this)
+    end
+end
+
+function Dead()
+
+end
+
+function OnTriggerEnter(Entity)
+    
+end
+
+function OnTrigger(Entity)
+    
+end
+
+function OnTriggerExit(Entity)
+    
+end
