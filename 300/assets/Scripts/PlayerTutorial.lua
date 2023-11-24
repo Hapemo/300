6d145e0caced9fe25e17b9fd0241a2c10abd0018
michellelor <xaunyunmichelle.lor@digipen.edu>
2023-11-24 16:58:56 +0800

    drag text tut scene

--- 300/assets/Scripts/PlayerTutorial.lua
+++ 300/assets/Scripts/PlayerTutorial.lua
@@ -308,12 +308,14 @@ function Update()
     -- meshtp3:SetColor(tpcolor)
     -- meshtp4:SetColor(tpcolor)
 
-   --dashrender = dashui:GetUIrenderer()
-    -- if (dashTime > 3.0) then
-    --     dashrender:SetDegree(0)
-    -- else
-    --     dashrender:SetDegree(360 - (dashTime / 3.0) * 360)
-    -- end
+    dashui = gameStateSys:GetEntityByScene("dashui" , "UI")
+
+   dashrender = dashui:GetUIrenderer()
+    if (dashTime > 3.0) then
+        dashrender:SetDegree(0)
+    else
+        dashrender:SetDegree(360 - (dashTime / 3.0) * 360)
+    end
 
     -- if (tpTime <= 20.0) then  
     --     graphicsSys.mSamplingWeight = graphicsSys.mSamplingWeight - FPSManager.GetDT()
