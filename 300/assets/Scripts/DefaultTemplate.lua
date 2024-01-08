102c81248374435c1b4afdd417848c00c8111b27
michellelor <xaunyunmichelle.lor@digipen.edu>
2024-01-08 13:45:16 +0800

    Update DefaultTemplate.lua
    :)!!!!!!!!!!!!!

--- 300/assets/Scripts/DefaultTemplate.lua
+++ 300/assets/Scripts/DefaultTemplate.lua
@@ -14,10 +14,16 @@ function OnTriggerEnter(Entity)
     
 end
 
-function OnTrigger(Entity)
-    
-end
-
 function OnTriggerExit(Entity)
     
 end
+
+function OnContactEnter(Entity)
+
+end
+
+function OnContactExit(Entity)
+
+end
+
+
