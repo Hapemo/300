5910aa2f3f93ff44b0bf839f2007b7bf407cb356
Hapemo <j.teoh@digipen.edu>
2024-01-20 19:01:30 +0800

    Sync trojan horse animation

--- 300/assets/Scripts/Melissa.lua
+++ 300/assets/Scripts/Melissa.lua
@@ -2,7 +2,7 @@
 local speed             = 3
 local damage            = 15
 
-local s2AttackingTimer          = 1
+local s2AttackingTimer          = 1.875 -- This is the attack animation length
 local s2AttackingTimerCount     = 0
 
 local s3ScreamingTimer           = 0
@@ -154,6 +154,7 @@ end
 -- State initialise functions
 function AttackInit()
     -- Start attack animation
+    this:GetMeshRenderer():SetMesh("Melissa_attack", this)
     AttackOnce = false
     s2AttackingTimerCount = 0
     state = "ATTACKING"
@@ -161,6 +162,9 @@ end
 
 function TRAVELInit()
     state = "TRAVEL"
+
+    this:GetMeshRenderer():SetMesh("Melissa_attack", this) -- Change back to idle animation 
+
 end
 
 function SpawnMelissa() -- This function should be called in 
