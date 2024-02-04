f255bcd743b54e4c5a9adb3e7cbded0290582a8c
saladpotato <cheongminglunwork@gmail.com>
2024-02-04 15:56:57 +0800

    (FIX) Adjusted Crosshair BUG

--- 300/assets/Scripts/Crosshair.lua
+++ 300/assets/Scripts/Crosshair.lua
@@ -1,6 +1,6 @@
 local d_position = Vec2.new(-102,102)
 local m_position = Vec2.new(-202,202)
-local de_speed = 10
+local de_speed = 0.05
 local add_speed = 1
 local ent
 local inputMapSys
@@ -22,46 +22,46 @@ function Update()
     if(inputMapSys:GetButton("Shoot")) then
         
         if  (general.name == "crosshair-1")then
-            if(transform.mTranslate.x > -202)then
+            if(transform.mTranslate.x > -0.1)then
                 transform.mTranslate.x =  transform.mTranslate.x -de_speed
                 transform.mTranslate.y =  transform.mTranslate.y +de_speed
             end
         elseif (general.name == "crosshair-2")then
-            if(transform.mTranslate.x < 202)then
+            if(transform.mTranslate.x < 0.1)then
                 transform.mTranslate.x =  transform.mTranslate.x +de_speed
                 transform.mTranslate.y =  transform.mTranslate.y +de_speed
             end
 
         elseif (general.name == "crosshair-3")then
-            if(transform.mTranslate.x < 202)then
+            if(transform.mTranslate.x < 0.1)then
                 transform.mTranslate.x =  transform.mTranslate.x +de_speed
                 transform.mTranslate.y =  transform.mTranslate.y -de_speed
             end
         elseif (general.name == "crosshair-4")then
-            if(transform.mTranslate.x > -202)then
+            if(transform.mTranslate.x > -0.1)then
                 transform.mTranslate.x =  transform.mTranslate.x -de_speed
                 transform.mTranslate.y =  transform.mTranslate.y -de_speed
             end
         end
     else
         if  (general.name == "crosshair-1")then
-            if(transform.mTranslate.x  < -102)then
+            if(transform.mTranslate.x  < -0.1)then
                 transform.mTranslate.x =  transform.mTranslate.x +de_speed
                 transform.mTranslate.y =  transform.mTranslate.y -de_speed
             end
 
         elseif (general.name == "crosshair-2")then
-            if(transform.mTranslate.x > 102)then
+            if(transform.mTranslate.x > 0.1)then
                 transform.mTranslate.x =  transform.mTranslate.x -de_speed
                 transform.mTranslate.y =  transform.mTranslate.y -de_speed
             end
         elseif (general.name == "crosshair-3")then
-            if(transform.mTranslate.x > 102)then
+            if(transform.mTranslate.x > 0.1)then
                 transform.mTranslate.x =  transform.mTranslate.x -de_speed
                 transform.mTranslate.y =  transform.mTranslate.y +de_speed
             end
         elseif (general.name == "crosshair-4")then
-            if(transform.mTranslate.x < -102)then
+            if(transform.mTranslate.x < -0.1)then
                 transform.mTranslate.x =  transform.mTranslate.x +de_speed
                 transform.mTranslate.y =  transform.mTranslate.y +de_speed
             end
