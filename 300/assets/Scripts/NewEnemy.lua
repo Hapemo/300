6808b68fcbb9cfe81b2b89d3be3fe394f37a4322
Hapemo <j.teoh@digipen.edu>
2024-02-04 14:39:47 +0800

    red enemy activating (buggy)

--- 300/assets/Scripts/NewEnemy.lua
+++ 300/assets/Scripts/NewEnemy.lua
@@ -1,18 +1,41 @@
 
+local name
+local this
+local target
+
+local aiSys
+local scriptingSys
+
+local runOnce = false
+
 function Alive()
     this = Helper.GetScriptEntity(script_entity.id)
     if this == nil then
         print("Entity nil in script!")
     end
+
+    name = string.lower(this:GetGeneral().name)
+    target = this:GetAISetting():GetTarget()
+    aiSys = systemManager:mAISystem();
+    scriptingSys = systemManager:mScriptingSystem();
 end
 
 function Update()
-        if this:GetHealthbar().health <= 0 then
-            --increase count of dead enemy
-            _G.PreObjectivesCounter = _G.PreObjectivesCounter + 1;
-            --transformed away fr now lol
-            systemManager.ecs:SetDeleteEntity(this)
-        end
+    if this:GetHealthbar().health <= 0 then
+        --increase count of dead enemy
+        _G.PreObjectivesCounter = _G.PreObjectivesCounter + 1;
+        --transformed away fr now lol
+        systemManager.ecs:SetDeleteEntity(this)
+    end
+
+    -- If can see player, activate the scripts accordingly
+    if not runOnce and (aiSys:ConeOfSight(this, target, 70, 40)) then
+        runOnce = true
+        print(name)
+        print("sees player")
+        ActivateScript(this)
+    end
+
 end
 
 function Dead()
@@ -35,4 +58,20 @@ function OnContactExit(Entity)
 
 end
 
-
+function ActivateScript(Entity)
+    local scriptName = ""
+    -- Check name of entity
+    if (string.find(name, "melissa")) then
+        scriptName = "..\\assets\\Scripts\\Melissa.lua"
+    elseif (string.find(name, "soldier")) then 
+        scriptName = "..\\assets\\Scripts\\TrojanSoldier.lua"
+    elseif (string.find(name, "horse")) then 
+        scriptName = "..\\assets\\Scripts\\TrojanHorse.lua"
+    elseif (string.find(name, "ily")) then 
+        scriptName = "..\\assets\\Scripts\\ILOVEYOU.lua"
+    elseif (string.find(name, "zipbomb")) then 
+        scriptName = "..\\assets\\Scripts\\ZipBomb.lua"
+    end
+    print(scriptName)
+    -- scriptingSys:AddScript(Entity, scriptName);
+end
