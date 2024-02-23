6551f413276a62f4cafd475efcbcd093d48e7b4a
Hapemo <j.teoh@digipen.edu>
2024-02-23 17:34:46 +0800

    midway trojan horse epic intro

---
+++ 300/assets/Scripts/EpicIntroManagerLv1.lua
@@ -0,0 +1,40 @@
+-- The main brain of epic intro for lv 1
+
+
+_G.gameStateSys = systemManager:mGameStateSystem()
+
+_G.activateTrojanHorse = false
+_G.activateTrojanSoldier = false
+_G.activateMelissa = false
+_G.activateILY = false
+_G.activateZipBomb = false
+
+function Alive()
+
+end
+
+function Update()
+    
+end
+
+function Dead()
+
+end
+
+function OnTriggerEnter(Entity)
+    
+end
+
+function OnTriggerExit(Entity)
+    
+end
+
+function OnContactEnter(Entity)
+
+end
+
+function OnContactExit(Entity)
+
+end
+
+
