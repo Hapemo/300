5d53ad4c25cc5bfa52fd2e1e30bde8c155b6af65
kewyj <74573283+kewyj@users.noreply.github.com>
2024-03-05 00:19:27 +0800

    level1 dialogues

--- 300/assets/Scripts/DialogueControllerLevel1.lua
+++ 300/assets/Scripts/DialogueControllerLevel1.lua
@@ -26,8 +26,6 @@ local CompleteObjective3_2Progress
 local DestroyedAV1Progress
 local DestroyedAV2Progress
 
-local currSubState
-
 local currState
 
 local scared
@@ -41,6 +39,11 @@ local frameOpened
 local frameClosed
 local finished
 
+local framespeed
+local picturespeed
+
+local counter
+local picture
 function Alive()
     Intro1 = gameStateSys:GetEntity("DialogueIntro1", "Dialogue_Level1")
     Intro2 = gameStateSys:GetEntity("DialogueIntro2", "Dialogue_Level1")
@@ -53,8 +56,8 @@ function Alive()
     CompleteObjective2 = gameStateSys:GetEntity("DialogueCompleteObjective2", "Dialogue_Level1")
     CompleteObjective3_1 = gameStateSys:GetEntity("DialogueCompleteObjective3_1", "Dialogue_Level1")
     CompleteObjective3_2 = gameStateSys:GetEntity("DialogueCompleteObjective3_2", "Dialogue_Level1")
-    EnemyDestroysAV1 = gameStateSys:GetEntity("DialogueEnemyDestroysAV1", "Dialogue_Level1")
-    EnemyDestroysAV2 = gameStateSys:GetEntity("DialogueEnemyDestroysAV2", "Dialogue_Level1")
+    DestroyedAV1 = gameStateSys:GetEntity("DialogueEnemyDestroysAV1", "Dialogue_Level1")
+    DestroyedAV2 = gameStateSys:GetEntity("DialogueEnemyDestroysAV2", "Dialogue_Level1")
 
     Intro1Progress = 0.0
     Intro2Progress = 0.0
@@ -77,16 +80,34 @@ function Alive()
     excited = gameStateSys:GetEntity("DialogueExcited", "Dialogue_Level1")
     normal = gameStateSys:GetEntity("DialogueNormal", "Dialogue_Level1")
     angry = gameStateSys:GetEntity("DialogueAngry", "Dialogue_Level1")
-
     frame = gameStateSys:GetEntity("DialogueFrame", "Dialogue_Level1")
 
     frameOpened = false
     frameClosed = false
     finish = false
+
+    speed = 0.01
+    framespeed = 0.25
+    picturespeed = 0.03125
+
+    counter = 0
+
+    picture = normal
 end
 
 function Update()
+    
     if currState ~= "" then
+        if currState == "loadin" then
+            picture = angry
+        elseif currState == "obj1" then
+            picture = excited
+        elseif currState == "obj2" then
+            picture = scared
+        elseif currState == "obj3" then
+            picture = angry
+        end
+
         if frameOpened == false then
             OpenFrame()
         end
@@ -97,6 +118,14 @@ function Update()
 
         if finish == true and frameClosed == false then
             CloseFrame()
+        end
+
+        if finish == true and frameClosed == true then
+            frameOpened = false
+            frameClosed = false
+            finish = false
+            currState = ""
+            counter = 0
         end
     end
 end
@@ -124,18 +153,191 @@ end
 function UpdateDialogues()
     if currState == "intro" then
         if inputMapSys:GetButtonDown("skip") then 
-            if Intro1Progress >= 1.0 then
+            if Intro2Progress >= 1.0 then
+                EndDialogue()
+                finish = true
             else
+                SkipAnimation()
+            end
+        end
+
+        if Intro1Progress < 1.0 then 
+            Intro1Progress = speed + Intro1Progress
+            if Intro1Progress > 1.0 then
+                Intro1Progress = 1.1
+                Intro1:GetUIrenderer():SetSlider(1.0)
+            else
+                Intro1:GetUIrenderer():SetSlider(Intro1Progress)
+            end
+        elseif Intro2Progress < 1.0 then
+            Intro2Progress = speed + Intro2Progress
+            if Intro2Progress > 1.0 then
+                Intro2Progress = 1.1
+                Intro2:GetUIrenderer():SetSlider(1.0)
+            else
+                Intro2:GetUIrenderer():SetSlider(Intro2Progress)
             end
         end
     elseif currState == "loadin" then
-        
+        if inputMapSys:GetButtonDown("skip") then 
+            if counter == 0 then
+                if LoadInProgress >= 1.0 then
+                    EndDialogue()
+                else
+                    SkipAnimation()
+                end
+            elseif counter == 1 then
+                if ObjectiveBrief1_2Progress >= 1.0 then
+                    EndDialogue()
+                else
+                    SkipAnimation()
+                end
+            elseif counter == 2 then
+                if ObjectiveBrief2_2Progress >= 1.0 then
+                    EndDialogue()
+                    finish = true
+                else
+                    SkipAnimation()
+                end
+            end
+        end
+        if counter == 0 then
+            if LoadInProgress < 1.0 then 
+                LoadInProgress = speed + LoadInProgress
+                if LoadInProgress > 1.0 then
+                    LoadInProgress = 1.1
+                    LoadIn:GetUIrenderer():SetSlider(1.0)
+                else
+                    LoadIn:GetUIrenderer():SetSlider(LoadInProgress)
+                end
+            end
+        elseif counter == 1 then
+            if ObjectiveBrief1_1Progress < 1.0 then 
+                ObjectiveBrief1_1Progress = speed + ObjectiveBrief1_1Progress
+                if ObjectiveBrief1_1Progress > 1.0 then
+                    ObjectiveBrief1_1Progress = 1.1
+                    ObjectiveBrief1_1:GetUIrenderer():SetSlider(1.0)
+                else
+                    ObjectiveBrief1_1:GetUIrenderer():SetSlider(ObjectiveBrief1_1Progress)
+                end
+            elseif ObjectiveBrief1_2Progress < 1.0 then
+                ObjectiveBrief1_2Progress = speed + ObjectiveBrief1_2Progress
+                if ObjectiveBrief1_2Progress > 1.0 then
+                    ObjectiveBrief1_2Progress = 1.1
+                    ObjectiveBrief1_2:GetUIrenderer():SetSlider(1.0)
+                else
+                    ObjectiveBrief1_2:GetUIrenderer():SetSlider(ObjectiveBrief1_2Progress)
+                end
+            end
+        elseif counter == 2 then
+            if ObjectiveBrief2_1Progress < 1.0 then 
+                ObjectiveBrief2_1Progress = speed + ObjectiveBrief2_1Progress
+                if ObjectiveBrief2_1Progress > 1.0 then
+                    ObjectiveBrief2_1Progress = 1.1
+                    ObjectiveBrief2_1:GetUIrenderer():SetSlider(1.0)
+                else
+                    ObjectiveBrief2_1:GetUIrenderer():SetSlider(ObjectiveBrief2_1Progress)
+                end
+            elseif ObjectiveBrief2_2Progress < 1.0 then
+                ObjectiveBrief2_2Progress = speed + ObjectiveBrief2_2Progress
+                if ObjectiveBrief2_2Progress > 1.0 then
+                    ObjectiveBrief2_2Progress = 1.1
+                    ObjectiveBrief2_2:GetUIrenderer():SetSlider(1.0)
+                else
+                    ObjectiveBrief2_2:GetUIrenderer():SetSlider(ObjectiveBrief2_2Progress)
+                end
+            end
+        end
     elseif currState == "obj1" then
-        
+        if inputMapSys:GetButtonDown("skip") then 
+            if CompleteObjective1Progress >= 1.0 then
+                EndDialogue()
+                finish = true
+            else
+                SkipAnimation()
+            end
+        end
+        if CompleteObjective1Progress < 1.0 then
+            CompleteObjective1Progress = speed + CompleteObjective1Progress
+            if CompleteObjective1Progress > 1.0 then
+                CompleteObjective1Progress = 1.1
+                CompleteObjective1:GetUIrenderer():SetSlider(1.0)
+            else
+                CompleteObjective1:GetUIrenderer():SetSlider(CompleteObjective1Progress)
+            end
+        end
     elseif currState == "obj2" then
-        
+        if inputMapSys:GetButtonDown("skip") then 
+            if CompleteObjective2Progress >= 1.0 then
+                EndDialogue()
+                finish = true
+            else
+                SkipAnimation()
+            end
+        end
+        if CompleteObjective2Progress < 1.0 then
+            CompleteObjective2Progress = speed + CompleteObjective2Progress
+            if CompleteObjective2Progress > 1.0 then
+                CompleteObjective2Progress = 1.1
+                CompleteObjective2:GetUIrenderer():SetSlider(1.0)
+            else
+                CompleteObjective2:GetUIrenderer():SetSlider(CompleteObjective2Progress)
+            end
+        end
     elseif currState == "obj3" then
-        
+        if inputMapSys:GetButtonDown("skip") then 
+            if counter == 0 then
+                if CompleteObjective3_2Progress >= 1.0 then
+                    EndDialogue()
+                else
+                    SkipAnimation()
+                end
+            elseif counter == 1 then
+                if DestroyedAV2Progress >= 1.0 then
+                    EndDialogue()
+                    finish = true
+                else
+                    SkipAnimation()
+                end
+            end
+        end
+        if counter == 0 then
+            if CompleteObjective3_1Progress < 1.0 then 
+                CompleteObjective3_1Progress = speed + CompleteObjective3_1Progress
+                if CompleteObjective3_1Progress > 1.0 then
+                    CompleteObjective3_1Progress = 1.1
+                    CompleteObjective3_1:GetUIrenderer():SetSlider(1.0)
+                else
+                    CompleteObjective3_1:GetUIrenderer():SetSlider(CompleteObjective3_1Progress)
+                end
+            elseif CompleteObjective3_2Progress < 1.0 then
+                CompleteObjective3_2Progress = speed + CompleteObjective3_2Progress
+                if CompleteObjective3_2Progress > 1.0 then
+                    CompleteObjective3_2Progress = 1.1
+                    CompleteObjective3_2:GetUIrenderer():SetSlider(1.0)
+                else
+                    CompleteObjective3_2:GetUIrenderer():SetSlider(CompleteObjective3_2Progress)
+                end
+            end
+        elseif counter == 1 then
+            if DestroyedAV1Progress < 1.0 then 
+                DestroyedAV1Progress = speed + DestroyedAV1Progress
+                if DestroyedAV1Progress > 1.0 then
+                    DestroyedAV1Progress = 1.1
+                    DestroyedAV1:GetUIrenderer():SetSlider(1.0)
+                else
+                    DestroyedAV1:GetUIrenderer():SetSlider(DestroyedAV1Progress)
+                end
+            elseif DestroyedAV2Progress < 1.0 then
+                DestroyedAV2Progress = speed + DestroyedAV2Progress
+                if DestroyedAV2Progress > 1.0 then
+                    DestroyedAV2Progress = 1.1
+                    DestroyedAV2:GetUIrenderer():SetSlider(1.0)
+                else
+                    DestroyedAV2:GetUIrenderer():SetSlider(DestroyedAV2Progress)
+                end
+            end
+        end
     end
 end
 
@@ -145,10 +347,10 @@ function OpenFrame()
         if frame:GetTransform().mScale.x > 1.98 then
             frame:GetTransform().mScale.x = 2.0
         end
-    elseif normal:GetTransform().mScale.x <= 0.24 then
-        normal:GetTransform().mScale.x = normal:GetTransform().mScale.x + picturespeed
-        if normal:GetTransform().mScale.x > 0.23 then
-            normal:GetTransform().mScale.x = 0.25
+    elseif picture:GetTransform().mScale.x <= 0.24 then
+        picture:GetTransform().mScale.x = picture:GetTransform().mScale.x + picturespeed
+        if picture:GetTransform().mScale.x > 0.23 then
+            picture:GetTransform().mScale.x = 0.25
         end
     else
         frameOpened = true
@@ -156,10 +358,13 @@ function OpenFrame()
 end
 
 function CloseFrame()
-    if normal:GetTransform().mScale.x >= 0.01 then
-        normal:GetTransform().mScale.x = normal:GetTransform().mScale.x - picturespeed
-        if normal:GetTransform().mScale.x < 0.0 then
-            normal:GetTransform().mScale.x = 0.0
+    if currState == "obj3" then 
+        picture = normal
+    end
+    if picture:GetTransform().mScale.x >= 0.01 then
+        picture:GetTransform().mScale.x = picture:GetTransform().mScale.x - picturespeed
+        if picture:GetTransform().mScale.x < 0.0 then
+            picture:GetTransform().mScale.x = 0.0
         end
     elseif frame:GetTransform().mScale.x >= 0.01 then
         frame:GetTransform().mScale.x = frame:GetTransform().mScale.x - framespeed
@@ -171,6 +376,84 @@ function CloseFrame()
     end
 end
 
+function SkipAnimation()
+    if currState == "intro" then
+        Intro1Progress = 1.1
+        Intro2Progress = 1.1
+        Intro1:GetUIrenderer():SetSlider(1.0)
+        Intro2:GetUIrenderer():SetSlider(1.0)
+    elseif currState == "loadin" then
+        if counter == 0 then
+            LoadInProgress = 1.1
+            LoadIn:GetUIrenderer():SetSlider(1.0)
+        elseif counter == 1 then
+            ObjectiveBrief1_1Progress = 1.1
+            ObjectiveBrief1_2Progress = 1.1
+            ObjectiveBrief1_1:GetUIrenderer():SetSlider(1.0)
+            ObjectiveBrief1_2:GetUIrenderer():SetSlider(1.0)
+        elseif counter == 2 then
+            ObjectiveBrief2_1Progress = 1.1
+            ObjectiveBrief2_2Progress = 1.1
+            ObjectiveBrief2_1:GetUIrenderer():SetSlider(1.0)
+            ObjectiveBrief2_2:GetUIrenderer():SetSlider(1.0)
+        end
+    elseif currState == "obj1" then
+        CompleteObjective1Progress = 1.1
+        CompleteObjective1:GetUIrenderer():SetSlider(1.0)
+    elseif currState == "obj2" then
+        CompleteObjective2Progress = 1.1
+        CompleteObjective2:GetUIrenderer():SetSlider(1.0)
+    elseif currState == "obj3" then
+        if counter == 0 then
+            CompleteObjective3_1Progress = 1.1
+            CompleteObjective3_2Progress = 1.1
+            CompleteObjective3_1:GetUIrenderer():SetSlider(1.0)
+            CompleteObjective3_2:GetUIrenderer():SetSlider(1.0)
+        elseif counter == 1 then
+            DestroyedAV1Progress = 1.1
+            DestroyedAV2Progress = 1.1
+            DestroyedAV1:GetUIrenderer():SetSlider(1.0)
+            DestroyedAV2:GetUIrenderer():SetSlider(1.0)
+        end
+    end
+end
+
+function EndDialogue()
+    if currState == "intro" then
+        Intro1:GetUIrenderer():SetSlider(0.0)
+        Intro2:GetUIrenderer():SetSlider(0.0)
+    elseif currState == "loadin" then
+        if counter == 0 then
+            LoadIn:GetUIrenderer():SetSlider(0.0)
+            counter = 1
+            ChangePicture(angry, normal)
+        elseif counter == 1 then
+            ObjectiveBrief1_1:GetUIrenderer():SetSlider(0.0)
+            ObjectiveBrief1_2:GetUIrenderer():SetSlider(0.0)
+            counter = 2
+            ChangePicture(normal, angry)
+        elseif counter == 2 then
+            ObjectiveBrief2_1:GetUIrenderer():SetSlider(0.0)
+            ObjectiveBrief2_2:GetUIrenderer():SetSlider(0.0)
+            counter = 3
+        end
+    elseif currState == "obj1" then
+        CompleteObjective1:GetUIrenderer():SetSlider(0.0)
+    elseif currState == "obj2" then
+        CompleteObjective2:GetUIrenderer():SetSlider(0.0)
+    elseif currState == "obj3" then
+        if counter == 0 then
+            CompleteObjective3_1:GetUIrenderer():SetSlider(0.0)
+            CompleteObjective3_2:GetUIrenderer():SetSlider(0.0)
+            counter = 1
+            ChangePicture(angry, normal)
+        elseif counter == 1 then
+            DestroyedAV1:GetUIrenderer():SetSlider(0.0)
+            DestroyedAV2:GetUIrenderer():SetSlider(0.0)
+            counter = 2
+        end
+    end
+end
 
 function FinishedCutscenes()
     currState = "loadin" --yellow
@@ -186,4 +469,10 @@ end
 
 function FinishObjective3()
     currState = "obj3" --light cyan
+end
+
+function ChangePicture(Entity1, Entity2) 
+    Entity1:GetTransform().mScale.x = 0
+    Entity2:GetTransform().mScale.x = 0.25
+    picture = Entity2
 end
