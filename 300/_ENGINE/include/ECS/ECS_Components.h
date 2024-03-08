1c0308198ba91895f80e53b6946226ca1b94d333
kewyj <74573283+kewyj@users.noreply.github.com>
2024-03-08 22:27:36 +0800

    Update ECS_Components.h

--- 300/_ENGINE/include/ECS/ECS_Components.h
+++ 300/_ENGINE/include/ECS/ECS_Components.h
@@ -581,7 +581,6 @@ struct Audio : public Serializable
 
 	void SetPlay(/*float vol = 1.0f*/)
 	{
-		std::cout << "playing audio: " << mFileName << std::endl;
 		mNextActionState = STATE::SET_TO_PLAY;
 		//mVolume = vol;
 	}
