032dcf7f250e5123d15fcf828e46de84d1c27727
kewyj <74573283+kewyj@users.noreply.github.com>
2024-03-31 17:51:13 +0800

    trojan no freeze

--- 300/assets/Scripts/EpicIntroTrojanHorseNoFreeze.lua
+++ 300/assets/Scripts/EpicIntroTrojanHorseNoFreeze.lua
@@ -1,4 +1,3 @@
--- This script is same as trojan horse but specially for epic intro only
 -- Variables for state
 local s1Timer           = 2
 local s1RoamVelocity    = Vec3.new()
@@ -6,13 +5,11 @@ local roamSpeed         = 2
 local damage            = 20
 
 local s2ChargeCount     = 0
-local s2Charge          = 1.5
+local s2Charge          = 2
 
 local s3SprintVelocity  = Vec3.new()
 local sprintSpeed       = 15
 local stareDirection    = Vec3.new()
-
-local dashStopTimerCount = 0
 
 local s4RestTimer       = 2
 local s4RestTimerCount  = 0
@@ -33,6 +30,11 @@ local spawnSoldierFrequency = 0.6
 local spawnSoldierCounter = 0
 
 local state
+
+local audio_played = false
+
+local isHit
+local countHit
 -- Trojan horse states
 -- 1. ROAM. roam around and passively look for player (change to 2. when sees player)
 -- 2. CHARGE. saw player, eyes glow red, play some charge up noise, delay about 3 seconds before charging to player (change to 3. when delay ends)
@@ -40,13 +42,15 @@ local state
 -- 4. REST. stops for around 0.5 seconds before moving back to 1. (change to 1. when rest timer ends)
 
 function Alive()
+    -- print("Alive()")
     math.randomseed(os.time())
 
     this = Helper.GetScriptEntity(script_entity.id)
     if this == nil then
         print("Entity nil in Trojan Horse script!")
     end
-    
+
+    aiSys = systemManager:mAISystem();
     phySys = systemManager:mPhysicsSystem();
     gameStateSys = systemManager:mGameStateSystem();
 
@@ -60,19 +64,54 @@ function Alive()
 
     deathTimerCount   = 0
     target = this:GetAISetting():GetTarget()
-
-    CHARGEInit()
+    isHit = false
+    countHit = 0.0
 end
 
 local timer = 0
 function Update()
-    if state == "CHARGE" then   -- saw player, eyes glow red, play some charge up noise, delay about 3 seconds before charging to player (change to 3. when delay ends)
+    ChangeColorOnHit()
+    -- If trojan horse epic intro is between state 4 and 6, return
+
+    -- dt = FPSManager.GetDT()
+
+    -- timer = timer - dt 
+
+    -- if (timer < 0) then
+    --     this:GetAudio():SetPlay()
+    --     timer = 3
+    -- end
+
+
+    -- OTHER UPDATE CODES
+    -- if systemManager:mInputActionSystem():GetButtonDown("Test2") then
+    --     this:GetHealthbar().health = this:GetHealthbar().health - 100
+    -- end
+
+    -- STATE MACHINE
+    if state == "ROAM" then         -- roam around and passively look for player (change to 2. when sees player)
+        -- Roam around randomly
+        s1Timer = s1Timer + FPSManager.GetDT()
+        Helper.SetRealRotate(this, Vec3.new(0,Helper.DirectionToAngle(this, s1RoamVelocity),0))
+
+        if s1Timer > 2 then
+            s1Timer = 0
+            MoveRandDir()
+        end
+        phySys:SetVelocity(this, s1RoamVelocity)
+
+        -- Look for player here
+        if aiSys:ConeOfSight(this, target, 70, 40) then
+            -- this:GetAudio():SetPlay()
+            CHARGEInit()
+        end
+
+    elseif state == "CHARGE" then   -- saw player, eyes glow red, play some charge up noise, delay about 3 seconds before charging to player (change to 3. when delay ends)
         -- Play animation for eyes glowing red        
         
-        -- make trojan horse dash towards z = 1
-        stareDirection = Vec3.new()
-        stareDirection.z = 1
-        Helper.SetRealRotate(this, Vec3.new(0, Helper.DirectionToAngle(this, stareDirection), 0))
+        -- Constantly make him stare at player and stand still
+        stareDirection = Helper.Vec3Minus(target:GetTransform().mTranslate, this:GetTransform().mTranslate)
+        Helper.SetRealRotate(this, Vec3.new(0,Helper.DirectionToAngle(this, stareDirection),0))
         phySys:SetVelocity(this, Vec3.new())
         
         -- if audio_played == false then 
@@ -86,33 +125,49 @@ function Update()
             SPRINTInit()
 
             s3SprintVelocity = Helper.Scale(Helper.Normalize(stareDirection), sprintSpeed)
-            s3SprintVelocity.y = -2
+            s3SprintVelocity.y = 0
             -- TODO End audio and animation
         end
 
     elseif state == "SPRINT" then   -- charge toward last seen player position at high speed (change to 4. when collided with something)
+        Helper.SetRealRotate(this, Vec3.new(0,Helper.DirectionToAngle(this, stareDirection),0))
+        if (_G.TrojanHorseEpicIntroState >= 5 and _G.TrojanHorseEpicIntroState <= 7) then return end
         -- Charge towards last seen player position at high speed
         phySys:SetVelocity(this, s3SprintVelocity);
-        Helper.SetRealRotate(this, Vec3.new(0, Helper.DirectionToAngle(this, stareDirection), 0))
         -- Stop and change state when collided with something
         -- This part is done in OnContactEnter
-        dashStopTimerCount = dashStopTimerCount + FPSManager.GetDT()
-        if dashStopTimerCount > _G.THdashStopTimer then
-            dashStopTimerCount = 0
-            
-            EndEpicIntro()
-        end
 
     elseif state == "REST" then     -- stops for some time before moving back to 1. (change to 1. when rest timer ends)
-        Helper.SetRealRotate(this, Vec3.new(0, Helper.DirectionToAngle(this, stareDirection), 0))
+        Helper.SetRealRotate(this, Vec3.new(0,Helper.DirectionToAngle(this, stareDirection),0))
         s4RestTimerCount = s4RestTimerCount + FPSManager.GetDT()
         if s4RestTimerCount > s4RestTimer then
             s4RestTimerCount = 0
-            EndEpicIntro()
+            ROAMInit()
         end
+    elseif state == "DEATH" then
+        -- spawning soldier
+        local dt = FPSManager.GetDT()
+        spawnSoldierCounter = spawnSoldierCounter + dt
+        deathTimerCount = deathTimerCount + dt
+
+        if spawnSoldierCounter > spawnSoldierFrequency then
+            spawnSoldierCounter = 0
+            SpawnSoldier()
+        end
+        if deathTimerCount > deathTimer then Die() end
+        return
     end
     -- END STATE MACHINE
 
+    -- Health logic
+    if this:GetHealthbar().health <= 0 then
+        StartDeath() 
+        if _G.Level3_Monsters ~= nil then 
+            if _G.Level3_Monsters == true then 
+                _G.number_left_in_level_3 = _G.number_left_in_level_3 - 1
+            end
+        end
+    end
 end
 
 function Dead()
@@ -187,11 +242,6 @@ function RESTInit()
     phySys:SetVelocity(this, Vec3.new())
 end
 
-function EndEpicIntro()
-    this:GetScripts():AddScript(this, "../assets/Scripts/EpicIntroTrojanHorseNoFreeze.lua")
-    this:GetScripts():DeleteScript("..\\assets\\Scripts\\EpicIntroTrojanHorse.lua");
-end
-
 
 -- Helper functions
 
@@ -206,7 +256,7 @@ end
 function MoveRandDir()
     s1RoamVelocity = RandDirectionXZ()
     s1RoamVelocity = Helper.Normalize(s1RoamVelocity)
-        Helper.SetRealRotate(this, Vec3.new(0, Helper.DirectionToAngle(this, s1RoamVelocity), 0))
+    Helper.SetRealRotate(this, Vec3.new(0,Helper.DirectionToAngle(this, s1RoamVelocity),0))
     s1RoamVelocity = Helper.Scale(s1RoamVelocity, roamSpeed)
 end
 
@@ -240,4 +290,26 @@ function CrashIntoPlayer()
     local velocity = Helper.Scale(Helper.Normalize(this:GetRigidBody().mVelocity), 100)
     velocity.y = 20
     phySys:SetVelocity(target, velocity)
+end
+
+function OnOtherTriggerEnter(Entity)
+    if Entity:GetGeneral().tagid ~= 2 and Entity:GetGeneral().tagid ~= 9 and Entity:GetGeneral().tagid ~= 10 and Entity:GetGeneral().tagid ~= 11 and Entity:GetGeneral().tagid~= 12 then
+        return
+    end
+    isHit = true
+end
+
+function ChangeColorOnHit()
+    this = Helper.GetScriptEntity(script_entity.id)
+    if isHit == true then
+        this:GetMeshRenderer():SetColor(Vec4.new(0.05,0.05,0.05,1))
+        if countHit < 0.1 then 
+            countHit = countHit + FPSManager.GetDT()
+        else 
+            isHit = false
+            countHit = 0
+        end
+    else
+        this:GetMeshRenderer():SetColor(Vec4.new(1,1,1,1))
+    end
 end
